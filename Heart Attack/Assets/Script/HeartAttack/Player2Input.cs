0b29677617b3586e5c22b447ddb87c1b922e2fd3
Conrad <conradte3@gmail.com>
2015-09-11 17:36:44 -0400

    starting on splitscreen and p2 input

---
+++ Heart Attack/Assets/Script/HeartAttack/Player2Input.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// Require a character controller to be attached to the same game object
+[RequireComponent(typeof(Player1Movement))]
+[AddComponentMenu("Character/FPS Input Controller")]
+
+public class Player2Input : MonoBehaviour
+{
+
+    public Player1Movement p1Movement;
+    public SimpleSmoothMouseLook p1Look;
+    public float horizontal;
+    public float vertical;
+    public float horizontalRight;
+    public float verticalRight;
+
+    void Awake()
+    {
+        p1Movement = GetComponent<Player1Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //horizontal = Input.GetAxis("Horizontal_P1");  //360 Controller
+        //vertical = Input.GetAxis("Vertical_P1");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        //horizontalRight = Input.GetAxis("HorizontalRight_P1");
+        //verticalRight = Input.GetAxis("VerticalRight_P1");
+        horizontalRight = -Input.GetAxis("Mouse X");
+        verticalRight = Input.GetAxis("Mouse Y");
+
+        p1Movement.MoveInput(horizontal, vertical);
+        p1Look.LookInput(horizontalRight, verticalRight);
+    }
+}
